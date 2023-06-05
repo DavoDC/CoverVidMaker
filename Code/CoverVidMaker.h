ed08309f2a498a050cd1272375e9b699d4752afa
David <45155292+DavoDC@users.noreply.github.com>
2023-06-05 12:11:25 +0800

    Added Audio File Check
    - Added MP3 file amount check
    - Sorted out TTD
    - Added hardcoded paths
    - Removed old README
    - Removed un-needed compilation configurations

--- Code/CoverVidMaker.h
+++ Code/CoverVidMaker.h
@@ -30,46 +30,14 @@ public:
 	// Constructor
 	CoverVidMaker();
 
-	// Analyse driver
-	//void analyse();
-
-	//// Settings
-	//void toggleSampleData();
- //   void setCutoff(double);
-	//void toggleColumnPrint();
 	
-
-
 private:
 	// ### Private fields
-	//// Use Sample Data
-	//bool useSampleData;
-
-	//// Percentage Cutoff Value
-	//double cutoff;
-
-	//// Column Printing
-	//bool columnPrint;
-
-	//// MatchList
-	//MatchList ml;
-
-	//// Number of matches
-	//int matchNum;
+	// Audio file paths list
+	StringV audioFilePaths;
 	
 
-
 	// ### Private methods
-	// # Helpers
 	//std::string getReplayPath();
-	//void printColumns(std::string, std::string, std::string);
-	//void printStatsLine(double, std::string, int);
-	//void printSettingsUpdate(std::string);
-	//void printStatsHeading(std::string);
-	//void printDateStats();
-
-	//// # Main Statistics Method
-	//template <typename Property>
-	//void printFreqStats(std::string, std::function<Property(Match)>);
 };
 
